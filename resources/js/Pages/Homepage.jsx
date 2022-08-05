a4a9a072e8168f26eeff3ebb708d74fa43c4d35e
virusphp <packercyber@gmail.com>
2022-08-05 11:32:48 +0700

    update

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -4,13 +4,16 @@ import { Link, Head } from "@inertiajs/inertia-react";
 export default function Homepage(props) {
     console.log(props);
     return (
-        <div className="flex justify-center items-center min-h-screen bg-neutral-600 text-white text-2xl">
+        <div className="flex justify-center items-center min-h-screen bg-slate-50">
             <Head title={props.title} />
             <div>
                 {props.news
                     ? props.news.map((data, i) => {
                           return (
-                              <div key={i}>
+                              <div
+                                  key={i}
+                                  className="p-4 m-2 bg-white text-black shadow-md rounded-md"
+                              >
                                   <p>{data.title}</p>
                                   <p>{data.description}</p>
                                   <p>{data.category}</p>
