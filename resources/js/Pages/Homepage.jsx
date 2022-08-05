146ea38ac80683d0fb47ada0bf775806a21a87d3
virusphp <packercyber@gmail.com>
2022-08-05 13:38:29 +0700

    handle berita kosong

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, Head } from "@inertiajs/inertia-react";
 import Navbar from "@/Components/Navbar";
+import NewsList from "@/Components/Homepage/NewsList";
 
 export default function Homepage(props) {
     console.log(props);
@@ -9,21 +10,7 @@ export default function Homepage(props) {
             <Head title={props.title} />
             <Navbar />
             <div>
-                {props.news
-                    ? props.news.map((data, i) => {
-                          return (
-                              <div
-                                  key={i}
-                                  className="p-4 m-2 bg-white text-black shadow-md rounded-md"
-                              >
-                                  <p className="text-2xl">{data.title}</p>
-                                  <p className="text-sm">{data.description}</p>
-                                  <p>{data.category}</p>
-                                  <p>{data.author}</p>
-                              </div>
-                          );
-                      })
-                    : "<p>Saat ini belum ada berita tersedia</p>"}
+                <NewsList news={props.news} />
             </div>
         </div>
     );
